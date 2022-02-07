f6555b0199d7f25be697fc1cfd15eb0bd577dad0
houda <benradi.houda@gmail.com>
2022-02-07 17:27:16 +0100

    tp

---
+++ footer.php
@@ -0,0 +1,13 @@
+            
+         <footer>
+             <a href="">Telephone :
+         <?php
+               footer("0773939593", "benradi.houda@gmail.com" ,"3 Avenue de la Rébrique " ,"PARIS");
+             ?>
+            </a>
+         </footer>                      
+              
+         
+</body>
+</html>
+<?php include "./fonction.php" ?>  ;
